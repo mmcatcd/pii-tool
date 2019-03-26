270aba8e8a8094b57e25d5a1734c157dd43a7944
rvailnaveed <naveedm@tcd.ie>
2019-03-26 14:14:10 +0000

    ehm

--- Project/PII_tool.py
+++ Project/PII_tool.py
@@ -44,15 +44,10 @@ def main():
         
         
     if filename.endswith('.sql'):
-<<<<<<< HEAD
+        print("")
         # sqlObj = sqlData()
         #sql_df = sqlObj.sqldb_to_df(self, host, user, password, database, table, rules_dict)
         #report_data = sqlObj.run(self, rules_dict, sql_df)
-=======
-        print("")
-        #sqlObj = sqlData()
-        #report_data = sqlObj.run(rules_dict, sql_df)
->>>>>>> rvailnaveed
         #sqlObj.write_report(report_data)
 
     if filename.endswith('.csv'):
